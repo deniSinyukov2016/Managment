31980a22a437d7508d805d7a9801f4b2a4185169
Denis Sinyukov <sinyukov.denis2015@yandex.ru>
2018-04-08 02:56:52 +0300

    view create

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -8,6 +8,16 @@
 require('./bootstrap');
 require('../../../node_modules/jquery/dist/jquery.min.js');
 require('./tabs');
+require('./custom/popper.js/popper.js');
+require('./custom/perfect-scrollbar/js/perfect-scrollbar.jquery.js');
+// require('./custom/moment/moment.js');
+require('./custom/d3/d3.js');
+// require('./custom/rickshaw/rickshaw.min.js');
+// require('./custom/gmaps/gmaps.js');
+require('./custom/chart.js/Chart.js');
+require('./custom/katniss.js');
+require('./custom/ResizeSensor.js');
+require('./custom/dashboard.js');
 
 window.Vue = require('vue');
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,77 +23,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
-    {{--<script>--}}
-        {{--(function($) {--}}
-            {{--$(function() {--}}
-                {{--$('ul.tabs__caption').on('click', 'li:not(.active)', function() {--}}
-                    {{--$(this)--}}
-                        {{--.addClass('active').siblings().removeClass('active')--}}
-                        {{--.closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');--}}
 
-                    {{--localStorage.setItem("myTabs", this);--}}
-                    {{--localStorage.getItem("myTabs");--}}
-                    {{--var object = localStorage.getItem("myTabs");--}}
-
-                   {{--console.log(object);--}}
-                    {{--// if(){--}}
-                    {{--//     $('a[href="' +lastTab+ '"]').click();--}}
-                    {{--// }--}}
-                    {{--//console.log(lastTab);--}}
-                   {{--// window.location.hash = $(this).attr("href");--}}
-
-                    {{--//window.onload = this();--}}
-                    {{--// if(window.onload){--}}
-                    {{--//     $(this).addClass('active');--}}
-                    {{--// }--}}
-                    {{--// var oldLocation = location;--}}
-                    {{--// location = newLocation;--}}
-                    {{--// console.log(location);--}}
-                {{--});--}}
-
-
-            {{--});--}}
-
-            {{--// window.onload = function () {--}}
-            {{--//     if('div.tabs__content')--}}
-            {{--//     //console.log($('ul.tabs__caption').find('li>a[href="'+window.location.hash+'"]').closest('li').index());--}}
-            {{--//     $('ul.tabs__caption li').each(function () {--}}
-            {{--//        $(this).removeClass('active');--}}
-            {{--//     });--}}
-            {{--//     var lastTab = $('ul.tabs__caption').find('li>a[href="'+window.location.hash+'"]').closest('li');--}}
-            {{--//     //console.log(lastTab);--}}
-            {{--//     var index = $(lastTab).index();--}}
-            {{--//     var tab = $(lastTab).attr('data-tab');--}}
-            {{--//     $('div.tabs__content').each(function () {--}}
-            {{--//         $(this).removeClass('active');--}}
-            {{--//     });--}}
-            {{--//--}}
-            {{--//     $(lastTab).addClass('active');--}}
-            {{--//     $('#tabs').find('.tabs__content[data-tab="'+tab +'"]').addClass('active');--}}
-            {{--//     //console.log(lastTab);--}}
-            {{--// }--}}
-            {{--// $(document).ready(function(){--}}
-            {{--//     $('ul.tabs__caption').each(function () {--}}
-            {{--//         var location = window.location.href;--}}
-            {{--//         var link = this.href;--}}
-            {{--//         if(location == link) {--}}
-            {{--//             $('ul.tabs__caption').addClass('active');--}}
-            {{--//         }--}}
-            {{--//         //console.log(this);--}}
-            {{--//--}}
-            {{--//     });--}}
-
-
-                {{--// $(window).onload( function(){--}}
-                {{--//     console.log(this);--}}
-                {{--// });--}}
-
-            {{--// });--}}
-
-
-        {{--})(jQuery);--}}
-
-    {{--</script>--}}
 
 </body>
 </html>
