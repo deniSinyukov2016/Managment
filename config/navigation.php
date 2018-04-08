608d434c3d55c49916f810f3dabc69c5f64a2c7c
Denis Sinyukov <sinyukov.denis2015@yandex.ru>
2018-04-08 14:59:56 +0300

    config  and view sidebar

---
+++ config/navigation.php
@@ -0,0 +1,41 @@
+<?php
+
+return [
+    'sidebar_dashboard' => [
+        'dashboard' => [
+            'title' => 'Dashboard',
+            'route' => 'sdfhsdf',
+            'icon'  => 'icon ion-ios-home-outline',
+        ],
+        'projects'  => [
+            'title' => 'Проекты',
+            'route' => 'sdfhsdf',
+            'icon'  => 'icon ion-ios-list-outline',
+            'submenu' => [
+                'my_projects'   => [
+                    'title' => 'Мои проекты',
+                    'route' => 'sdfhsdf',
+                ],
+                'all_projects'   => [
+                    'title' => 'Все проекты',
+                    'route' => 'sdfhsdf',
+                ],
+            ]
+        ],
+        'users'     => [
+            'title' => 'Пользователи',
+            'route' => 'sdfhsdf',
+            'icon'  => 'icon ion-ios-list-outline',
+        ],
+        'ideas'     => [
+            'title' => 'Идеи',
+            'route' => 'sdfhsdf',
+            'icon'  => 'icon ion-ios-list-outline',
+        ],
+        'technologies'  => [
+            'title' => 'Технологии',
+            'route' => 'sdfhsdf',
+            'icon'  => 'icon ion-ios-list-outline',
+        ]
+    ]
+];
